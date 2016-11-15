bb71d92d5ffad7b9dea805c2c32c471ffd7c4b95
Carling Kirk <carlingkirk@gmail.com>
2016-11-14 16:07:25 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TeamPowered
+This project is to help manage teams.
---
+++ TeamPowered/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamPowered
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+
+        public void LoadPeople()
+        {
+
+        }
+    }
+}
